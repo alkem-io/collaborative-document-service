4f9bf9c2a02c3168c6dd2a5bce0ff61c2f2dbad7
Svetoslav <svetoslav@alkem.io>
2025-09-09 15:11:33 +0300

    fixed tests

--- src/hocuspocus/extensions/north-star-metric/north.star.metric.service.spec.ts
+++ src/hocuspocus/extensions/north-star-metric/north.star.metric.service.spec.ts
@@ -1,14 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { beforeEach, describe, expect, it } from 'vitest';
 import { NorthStarMetricService } from './north.star.metric.service';
+import { defaultMockerFactory } from '@test/utils';
 
-describe('NorthStartMetricService', () => {
+describe('NorthStarMetricService', () => {
   let service: NorthStarMetricService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [NorthStarMetricService],
-    }).compile();
+    })
+      .useMocker(defaultMockerFactory)
+      .compile();
 
     service = module.get<NorthStarMetricService>(NorthStarMetricService);
   });
--- test/utils/default.mocker.factory.ts
+++ test/utils/default.mocker.factory.ts
@@ -5,7 +5,12 @@ import { vi } from 'vitest';
  * You can add already existing providers here, to be auto mocked
  */
 const mockerDictionary = new Map<InjectionToken, any>();
-
+/**
+ * Use when you don't want a specific mock for your test.
+ * A default mocker factory for NestJS testing module. It auto mocks all class methods with vi.fn().
+ * It will mock class methods with vi.fn() and look for existing providers in the mockerDictionary.
+ * @param token
+ */
 export const defaultMockerFactory = (token: InjectionToken | undefined) => {
   if (typeof token === 'function') {
     // Mock all class methods with vi.fn()
